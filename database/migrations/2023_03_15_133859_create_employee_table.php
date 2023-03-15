5e78bf9827b6227f4e3fef9888a36e424432c498
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-03-15 10:54:54 -0300

    #7-release: criando migrations das tabelas de ponto

---
+++ database/migrations/2023_03_15_133859_create_employee_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->integer('age');
+            $table->string('office');
+            $table->integer('resp_adm_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee');
+    }
+};
---
+++ database/migrations/2023_03_15_133905_create_attendence_point_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendence_point', function (Blueprint $table) {
+            $table->id();
+            $table->integer('employee_id');
+            $table->integer('resp_adm_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendence_point');
+    }
+};
